1a660cbf486bb668134f05c184d69f60a4b8708c
Pugal-M <159568683+Pugal-M@users.noreply.github.com>
2025-02-07 20:31:02 +0530

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 
 from flask import Flask,request,jsonify # type: ignore
-
+from flask_cors import CORS
 app = Flask (__name__)
-
+CORS(app)
 @app.route('/api', methods = ['GET'])
  
 def returnascii():
